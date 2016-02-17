a1e4d5b8a6ce203ae49618d5e109dda44d5e3975
Dariush Hachemi <ufemf@student.kit.edu>
2016-02-17 16:51:04 +0100

    [common/tests] test_shiplist.py Comments are added

--- common/tests/test_shiplist.py
+++ common/tests/test_shiplist.py
@@ -9,7 +9,10 @@ class TestShipList(unittest.TestCase):
 	FIELDLENGTH = 16
 
 	def test_borderCollisionWhilePlacingShip(self):
-		ships = ShipList(self.FIELDLENGTH)  # Valid is 0<=x<=15 AND 0<=y<=15
+		"""
+		Checks that the x und y are in border: 0<=x<=15 AND 0<=y<=15		
+		"""		
+		ships = ShipList(self.FIELDLENGTH)  
 
 		ships.add(Field(-1,  3), Field(2 ,  3))
 		ships.add(Field(14,  3), Field(16,  3))
@@ -26,24 +29,33 @@ class TestShipList(unittest.TestCase):
 		self.assertEqual(ships.getShipCount(), 0)
 	
 	def test_diagonalPlacingShip(self):
+		"""
+		If the ship is diagonal: not(x1=x2 or y1=y2) the return false		
+		"""
 		ships = ShipList(self.FIELDLENGTH)  # Invalid: not (x1=x2 or y1=y2)
-
 		ships.add(Field(2,  3), Field(3 ,  4))
 		self.assertEqual(ships.getShipCount(), 0)
 
 	def test_addTooLongShip(self):
+		"""
+		If the ship is too long: ShipLenght>5 then return false		
+		"""		
 		ships = ShipList(self.FIELDLENGTH)  # Invalid: ShipLenght>5
-
 		ships.add(Field(2,  3), Field(2 ,  8))
 		self.assertEqual(ships.getShipCount(), 0)
 	
 	def test_addTooShortShip(self):
+		"""
+		If the ship is too short: ShipLenght<2 then return false 		
+		"""
 		ships = ShipList(self.FIELDLENGTH)  # Invalid: ShipLenght<2
-
 		ships.add(Field(2,  3), Field(2 ,  3))
 		self.assertEqual(ships.getShipCount(), 0)
 
 	def test_collidingShips(self):
+		"""
+		Checks that the ships don't collide with each other		
+		"""		
 		ships = ShipList(self.FIELDLENGTH)
 		ships.add(Field(3, 4), Field(6, 4))  # x1=3,x2=6, y1=y2=4 
 		
@@ -62,6 +74,9 @@ class TestShipList(unittest.TestCase):
 		self.assertEqual(ships.getShipCount(), 1)
 
 	def test_addToManyCarriers(self):
+		"""
+		Checks that the count of Carriers is one: ships.getCarrierCount()=1	
+		"""
 		ships = ShipList(self.FIELDLENGTH) # count of Carrier must be 1
 
 		ships.add(Field(0, 0), Field(0, 4))
@@ -71,6 +86,9 @@ class TestShipList(unittest.TestCase):
 		self.assertEqual(ships.getCarrierCount(), 1)
 
 	def test_addToManyBattleships(self):
+		"""
+		Checks that the count of Battleships is two: ships.getBattleshipCount()=2	
+		"""		
 		ships = ShipList(self.FIELDLENGTH) #count of Battleships must be 2
 
 		ships.add(Field(0, 0), Field(0, 3))
@@ -81,6 +99,9 @@ class TestShipList(unittest.TestCase):
 		self.assertEqual(ships.getBattleshipCount(), 2)
 
 	def test_addToManyCruisers(self):
+		"""
+		Checks that the count of Cruisers is three: ships.getCruiserCount()=3	
+		"""
 		ships = ShipList(self.FIELDLENGTH) #count of Cruisers must be 3
 
 		ships.add(Field(0, 0), Field(0, 2))
@@ -92,6 +113,9 @@ class TestShipList(unittest.TestCase):
 		self.assertEqual(ships.getCruiserCount(), 3)
 
 	def test_addToManyDestroyers(self):
+		"""
+		Checks that the count of Destroyers is four: ships.getDestroyerCount()=4	
+		"""
 		ships = ShipList(self.FIELDLENGTH) #count of Destroyers must be 4
 
 		ships.add(Field(0, 0), Field(0, 1))
